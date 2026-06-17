ec4b891b2c5a1fc3d4ec63d5e4fe5487caab30cf
ii-inkchoi <inkchoidesign3@gmail.com>
2026-06-17 13:24:56 -0400

    landing page update

--- landing page/src/src/landing-main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Landing from './Landing'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Landing />
-  </StrictMode>
-)
